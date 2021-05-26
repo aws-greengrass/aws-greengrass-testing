03a5ca834bac4be8f7d3b39ad06750613446fb1c
philcali <philip.cali@gmail.com>
2021-05-26 15:17:24 -0400

    Resource cleanup.

--- aws-greengrass-testing-launcher/src/main/java/com/aws/greengrass/testing/launcher/TestLauncher.java
+++ aws-greengrass-testing-launcher/src/main/java/com/aws/greengrass/testing/launcher/TestLauncher.java
@@ -3,7 +3,10 @@ package com.aws.greengrass.testing.launcher;
 import org.junit.platform.console.options.CommandLineOptions;
 import org.junit.platform.console.options.Details;
 import org.junit.platform.console.options.Theme;
+import org.junit.platform.console.tasks.ConsoleTestExecutor;
+import org.junit.platform.launcher.listeners.TestExecutionSummary;
 
+import java.io.PrintWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
@@ -16,7 +19,7 @@ public final class TestLauncher {
     private static final String DEFAULT_GLUE_PATH = "com.aws.greengrass";
     private static final String DEFAULT_FEATURES = "greengrass/features";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         final Path output;
         if (args.length > 0) {
             output = Paths.get(args[0]);
@@ -32,11 +35,19 @@ public final class TestLauncher {
             options.setIncludedTagExpressions(Arrays.asList(tags));
         }
         options.setSelectedClasspathResources(Arrays.asList(System.getProperty("feature.path", DEFAULT_FEATURES)));
+        final Path resultsXml = output.toAbsolutePath().resolve("TEST-greengrass-results.xml");
         options.setConfigurationParameters(new HashMap<String, String>() {{
            put("cucumber.glue", System.getProperty("glue.package", DEFAULT_GLUE_PATH));
            put("cucumber.plugin", new StringJoiner(",")
-                   .add("junit:" + output.toAbsolutePath().resolve("TEST-greengrass-results.xml").toString())
+                   .add("junit:" + resultsXml.toString())
                    .toString());
         }});
+        final ConsoleTestExecutor executor = new ConsoleTestExecutor(options);
+        final TestExecutionSummary summary = executor.execute(new PrintWriter(System.out));
+        if (summary.getTestsFailedCount() > 0) {
+            System.err.println("Test Failure: " + summary.getTestsFailedCount());
+            System.err.println("See the full error report: " + resultsXml);
+            System.exit(1);
+        }
     }
 }
---
+++ aws-greengrass-testing-modules/src/main/java/com/aws/greengrass/testing/modules/AWSResourceCleanupModule.java
@@ -0,0 +1,84 @@
+package com.aws.greengrass.testing.modules;
+
+import com.google.auto.service.AutoService;
+import com.google.inject.AbstractModule;
+import com.google.inject.Module;
+import com.google.inject.matcher.Matchers;
+import com.google.inject.spi.ProvisionListener;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+@AutoService(Module.class)
+public class AWSResourceCleanupModule extends AbstractModule {
+    private static final Logger LOGGER = LogManager.getLogger(AWSResourceCleanupModule.class);
+
+    private static class CleanupRunnable implements Runnable {
+        final Set<Closeable> closers;
+
+        CleanupRunnable(final Set<Closeable> closers) {
+            this.closers = closers;
+        }
+
+        @Override
+        public void run() {
+            LOGGER.info("Cleaning up orphaned resources");
+            for (Closeable closer : closers) {
+                try {
+                    closer.close();
+                    LOGGER.info("Cleaned up {}", closer);
+                } catch (IOException ie) {
+                    LOGGER.error("Failed to clean up {}", closer, ie);
+                }
+            }
+        }
+    }
+
+    private static class CleanupProvisioner implements ProvisionListener {
+        final Set<Closeable> closers;
+
+        CleanupProvisioner(final Set<Closeable> closers) {
+            this.closers = closers;
+        }
+
+        @Override
+        public <T> void onProvision(ProvisionInvocation<T> provisionInvocation) {
+            final T instance = provisionInvocation.provision();
+            if (instance instanceof Closeable && closers.add((Closeable) instance)) {
+                LOGGER.info("Provisioning {} for cleanup", instance.getClass());
+            }
+        }
+    }
+
+    private static class CleanupInterceptor implements MethodInterceptor {
+        final Set<Closeable> closers;
+
+        CleanupInterceptor(final Set<Closeable> closers) {
+            this.closers = closers;
+        }
+
+        @Override
+        public Object invoke(MethodInvocation methodInvocation) throws Throwable {
+            LOGGER.debug("Cleanup interceptor found {}", methodInvocation.getMethod());
+            if (methodInvocation.getMethod().getName().equals("close") && closers.remove(methodInvocation.getThis())) {
+                LOGGER.debug("{} removed a resource", methodInvocation.getMethod().getDeclaringClass());
+            }
+            return methodInvocation.proceed();
+        }
+    }
+
+    @Override
+    protected void configure() {
+        final Set<Closeable> closers = new ConcurrentSkipListSet<>();
+        final MethodInterceptor cleanup = new CleanupInterceptor(closers);
+        Runtime.getRuntime().addShutdownHook(new Thread(new CleanupRunnable(closers)));
+        bindListener(Matchers.any(), new CleanupProvisioner(closers));
+        bindInterceptor(Matchers.subclassesOf(Closeable.class), Matchers.any(), cleanup);
+    }
+}
