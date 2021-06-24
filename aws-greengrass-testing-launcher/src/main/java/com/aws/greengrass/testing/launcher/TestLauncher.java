5d1bc3c4e743efbb7a086aca4e2ac4bba078c4b7
Philip Cali <philip.cali@gmail.com>
2021-06-24 13:39:18 -0400

    chore: add and apply license plugin

--- aws-greengrass-testing-launcher/src/main/java/com/aws/greengrass/testing/launcher/TestLauncher.java
+++ aws-greengrass-testing-launcher/src/main/java/com/aws/greengrass/testing/launcher/TestLauncher.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package com.aws.greengrass.testing.launcher;
 
 import com.aws.greengrass.testing.launcher.reporting.StepTrackingReporting;
