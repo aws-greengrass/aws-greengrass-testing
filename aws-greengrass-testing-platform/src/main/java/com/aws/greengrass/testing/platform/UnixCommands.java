0702220c7c76f0c1e55f1811440046763d6e597f
urvashijain18 <urvajain@88665a323830.ant.amazon.com>
2021-08-24 14:13:55 -0700

    fix: map child to parent processes

--- aws-greengrass-testing-platform/src/main/java/com/aws/greengrass/testing/platform/UnixCommands.java
+++ aws-greengrass-testing-platform/src/main/java/com/aws/greengrass/testing/platform/UnixCommands.java
@@ -9,20 +9,25 @@ import com.aws.greengrass.testing.api.device.Device;
 import com.aws.greengrass.testing.api.device.exception.CommandExecutionException;
 import com.aws.greengrass.testing.api.device.model.CommandInput;
 import com.aws.greengrass.testing.api.device.model.PlatformOS;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.StringJoiner;
-// import java.util.regex.Matcher;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public abstract class UnixCommands implements Commands, UnixPathsMixin {
+    private static final Logger LOGGER = LogManager.getLogger(UnixCommands.class);
     private static final Pattern PID_REGEX = Pattern.compile("\\((\\d+)\\)");
     protected final Device device;
     private final PlatformOS host;
@@ -83,14 +88,34 @@ public abstract class UnixCommands implements Commands, UnixPathsMixin {
     }
 
     protected Map<Integer, List<Integer>> findDirectDecendants() throws CommandExecutionException {
-        CommandInput.builder().line("sudo script -c \"ls /proc\" processes_info.txt").build();
-        final String ppids = executeToString(CommandInput.builder().line("cat processes_info.txt").build());
-        System.out.println(ppids);
-        // final Matcher matcher = PID_REGEX.matcher(line);
+        //CommandInput.builder().line("sudo script -c \"ls /proc\" processes_info.txt").build();
+        final String ppids = executeToString(CommandInput.builder().line("ls /proc").build());
+        LOGGER.info("parent pids: " + ppids);
+        List<Integer> parentPids = Arrays.stream(ppids.split("\\r?\\n")).map(String::trim).flatMap(line -> {
+            final Matcher matcher = PID_REGEX.matcher(line);
+            final List<Integer> pids = new ArrayList<>();
+            while (matcher.find()) {
+                pids.add(Integer.parseInt(matcher.group(1).trim()));
+            }
+            return pids.stream();
+        }).collect(Collectors.toList());
         final Map<Integer, List<Integer>> pidMapping = new HashMap<>();
-        // while (matcher.find()) {
-        //     pidMapping.put(Integer.parseInt(matcher.group(1).trim()), new ArrayList<Integer>());
-        // }
+        for (int pid: parentPids) {
+            final String children = executeToString(CommandInput.builder().line("ls /proc/" + pid + "/task").build());
+            List<Integer> childPids = Arrays.stream(children.split("\\r?\\n")).map(String::trim).flatMap(line -> {
+                final Matcher matcher = PID_REGEX.matcher(line);
+                final List<Integer> pids = new ArrayList<>();
+                while (matcher.find()) {
+                    pids.add(Integer.parseInt(matcher.group(1).trim()));
+                }
+                return pids.stream();
+            }).collect(Collectors.toList());
+            if (!pidMapping.containsKey(pid)) {
+                pidMapping.put(pid, childPids);
+            } else {
+                //check if value against pid is same as childPids. If not then update the value and store in the map.
+            }
+        }
         return pidMapping;
     }
 }
