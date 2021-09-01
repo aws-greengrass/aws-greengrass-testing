2c9c22f0d423b41fe699b127f037429342d307fc
urvashijain18 <urvajain@88665a323830.ant.amazon.com>
2021-09-01 13:46:14 -0700

    fix: if condition to check no orphan processes exist after killing loader process

--- aws-greengrass-testing-platform/src/main/java/com/aws/greengrass/testing/platform/UnixCommands.java
+++ aws-greengrass-testing-platform/src/main/java/com/aws/greengrass/testing/platform/UnixCommands.java
@@ -9,9 +9,12 @@ import com.aws.greengrass.testing.api.device.Device;
 import com.aws.greengrass.testing.api.device.exception.CommandExecutionException;
 import com.aws.greengrass.testing.api.device.model.CommandInput;
 import com.aws.greengrass.testing.api.device.model.PlatformOS;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -19,10 +22,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Queue;
 import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
 public abstract class UnixCommands implements Commands, UnixPathsMixin {
+    private static final Logger LOGGER = LogManager.getLogger(UnixCommands.class);
     protected final Device device;
     private final PlatformOS host;
 
@@ -69,28 +74,44 @@ public abstract class UnixCommands implements Commands, UnixPathsMixin {
 
     @Override
     public List<Integer> findDescendants(int pid) throws CommandExecutionException {
-        return findDirectDescendants().get(pid);
+        Map<Integer, List<Integer>> pidMap = findDirectDescendants();
+        List<Integer> child = new ArrayList<>();
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(pid);
+        while (!queue.isEmpty()) {
+            int process = queue.poll();
+            child.add(process);
+            if (pidMap.get(process) != null) {
+                pidMap.get(process).stream().forEach(processId -> queue.add(processId));
+            }
+        }
+        return child;
     }
 
     @Override
     public void kill(List<Integer> processIds) throws CommandExecutionException {
         execute(CommandInput.builder()
-                .line("kill " + processIds.stream()
-                        .map(i -> Integer.toString(i))
-                        .collect(Collectors.joining(" ")))
-                .build());
+            .line("kill " + processIds.stream()
+                    .map(i -> Integer.toString(i))
+                    .collect(Collectors.joining(" ")))
+            .build());
     }
 
     protected Map<Integer, List<Integer>> findDirectDescendants() throws CommandExecutionException {
         // TODO: replace all system FS commands with a platform independent solution
-        final Map<Integer, List<Integer>> pidMapping = new HashMap<>();
+        Map<Integer, List<Integer>> pidMapping = new HashMap<>();
         // check if process has any child process running
         // Example Output: /proc/<childPID>/status:PPid: <parentPID>
         final String processIds = executeToString(CommandInput.builder()
-                .line("find /proc/ -name 'status' -maxdepth 2 -exec grep PPid /dev/null {} \\;").build());
+                .line("find /proc/ -name 'status' -maxdepth 2 -exec grep PPid /dev/null 2>&1 {} \\; || true")
+                .build());
         List<String> processes = Arrays.stream(processIds.split("\\r?\\n")).map(String::trim)
                 .collect(Collectors.toList());
         for (String process : processes) {
+            if (process.contains("No such file or directory")) {
+                LOGGER.info("Process {} no longer exists", process.substring(13, 18));
+                continue;
+            }
             String[] ppid = process.split("\\s");
             int childId = Integer.parseInt(ppid[0].split("/")[2]);
             int parentId = Integer.parseInt(ppid[1]);
