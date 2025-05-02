563f4da327bee0ee88ddda9d6b3a2371fc70b7cc
Patrick Cook <cookpate@amazon.com>
2025-05-02 15:55:05 -0700

    Add mqtt security tests

--- src/aws-greengrass-testing-security.py
+++ src/aws-greengrass-testing-security.py
@@ -1,4 +1,4 @@
-import json
+from typing import List, Tuple
 from pytest import fixture, mark
 from src.GGTestUtils import GGTestUtils
 from src.SystemInterface import SystemInterface
@@ -33,60 +33,35 @@ def system_interface():
     pass
 
 
-#TODO: INCOMPLETE only partially implemented
-# Scenario Outline: Security-6-T2-mqtt: As a service owner, I want to specify which components can publish on which mqtt topic
-def test_Security_6_T2_mqtt(gg_util_obj: GGTestUtils,
-                            system_interface: SystemInterface):
-    #   When I install the component IotMqttPublisher version 0.0.0 from local store with configuration
-    #     | value                                                                                                                                                                                                                                                        |
-    #     | {"MERGE":{"accessControl":{"aws.greengrass.ipc.mqttproxy":{"policyId1":{"policyDescription":"access to publish to mqtt topics","operations":["aws.greengrass#PublishToIoTCore"],"resources":["<resource>"]}}},"topic":"<topic>","QOS":"1","payload":"test"}} |
-    IotMqttPublisher_cloud_name = gg_util_obj.upload_component_with_versions(
-        "IotMqttPublisher", ["0.0.0"])
-    if IotMqttPublisher_cloud_name is None:
-        raise RuntimeError(
-            "Fatal error: IotMqttPublisher_cloud_name cannot be uploaded to cloud"
-        )
+ACL_TEST_TOPICS: List[Tuple[str, str, bool]] = [
+    (r"test/topic", r"test/topic", True),
+    (r"testStarWildcard/topic*suffix/match*",
+     r"testStarWildcard/topic-A/B/C-suffix/match-star/1/2", True),
+    # These escape sequences look to be not supported by lite
+    # (r"test${?}escape${$}char*", r"test?escape$char1/2/3", True),
+    (r"wrong/topic", r"test/topic", False),
+    (r"testStarWildcard/topic*suffix/no-mat:ch*",
+     r"testStarWildcard/topic-A/B/C-suffix/match-star/1/2", False),
+    # (r"test${?}escape\${$}char", r"test${?}escape${$}char", False)
+]
 
-    #TODO: MOdify this to use the python pubsub assertion component.
-    deployment_id = gg_util_obj.create_deployment(
-        gg_util_obj.get_thing_group_arn(config.thing_group_1), [
-            IotMqttPublisher_cloud_name +
-            ("{\"accessControl\":{\"aws.greengrass.ipc.mqttproxy\":{\"policyId1\":{\"policyDescription\":\"access to publish to mqtt topics\",\"operations\":[\"aws.greengrass#PublishToIoTCore\"],\"resources\":[\"<resource>\"]}}},\"topic\":\"<topic>\",\"QOS\":\"1\",\"payload\":\"test\"}",
-             )
-        ], "FirstDeployment")["deploymentId"]
+MQTT_TEST_TOPICS: List[Tuple[str, str, bool]] = [
+    (r"topic-A/B-suffix/+/level/#",
+     r"topic-A/B-suffix/matchPlus/level/match/pound", True),
+    (r"topic-A/B-suffix/+/level/#",
+     r"topic-A/B-suffix/matchPlus/extraLevel/level/match/pound", False)
+    # Lite doesn't support mixing wildcards from both ACL and MQTT topic syntax.
+    # (r"topic*suffix/+/level/#", r"topic-A/B-suffix/matchPlus/level/match/pound",
+    #  True),
+    # (r"topic*suffix/+/level/#",
+    #  r"topic-A/B-suffix/matchPlus/extraLevel/level/match/pound", False)
+]
 
-    assert (gg_util_obj.wait_for_deployment_till_timeout(
-        180, deployment_id) == "SUCCEEDED")
-
-    #   Then I can check the cli to see the status of component IotMqttPublisher is RUNNING
-    assert system_interface.check_systemctl_status_for_component(
-        IotMqttPublisher_cloud_name[0]) == "RUNNING"
-
-
-#   And I get 1 assertion with context "Successfully published to IoT topic <topic>"
-#   Examples:
-#     | resource                             | topic                                              |
-#     | test/topic                           | test/topic                                         |
-#     | testStarWildcard/topic*suffix/match* | testStarWildcard/topic-A/B/C-suffix/match-star/1/2 |
-#     | topic*suffix/+/level/#               | topic-A/B-suffix/matchPlus/level/match/pound       |
-#     | test\${?}escape\${$}char*            | test?escape$char1/2/3                              |
 
 
 # Scenario: Security-6-T2 & Security-6-T3
 # As a service owner, I want to specify which components can and cannot publish on which topic.
-@mark.parametrize(
-    "resource,topic,accepted",
-    [
-        (r"test/topic", r"test/topic", True),
-        (r"testStarWildcard/topic*suffix/match*",
-         r"testStarWildcard/topic-A/B/C-suffix/match-star/1/2", True),
-    # Not supported by lite
-    # ( r"test${?}escape${$}char*"               , r"test?escape$char1/2/3"                              , True  ),
-        (r"wrong/topic", r"test/topic", False),
-        (r"testStarWildcard/topic*suffix/no-match*",
-         r"testStarWildcard/topic-A/B/C-suffix/match-star/1/2", False),
-    # ( r"test${?}escape\${$}char"                , r"test${?}escape${$}char"                           , False )
-    ])
+@mark.parametrize("resource,topic,accepted", ACL_TEST_TOPICS)
 def test_Security_T2_Security_T3(gg_util_obj: GGTestUtils,
                                  system_interface: SystemInterface,
                                  resource: str, topic: str, accepted: bool):
@@ -103,9 +78,9 @@ def test_Security_T2_Security_T3(gg_util_obj: GGTestUtils,
         merge_config={
             "accessControl": {
                 "aws.greengrass.ipc.pubsub": {
-                    "policyId1": {
+                    "HelloWorldPubSub:pubsub:1": {
                         "policyDescription":
-                        "access to publish to mqtt topics",
+                        "access to publish to local topics",
                         "operations": [
                             "aws.greengrass#PublishToTopic",
                             "aws.greengrass#SubscribeToTopic"
@@ -115,13 +90,58 @@ def test_Security_T2_Security_T3(gg_util_obj: GGTestUtils,
                 }
             },
             "Topic": topic,
-            "QOS": "1",
-            "payload": payload
+            "Message": payload
         })
 
     deployment_id = gg_util_obj.create_deployment(
         thingArn=gg_util_obj.get_thing_group_arn(config.thing_group_1),
         component_list=[pubsub_cloud_name],
+        deployment_name="FirstDeployment")["deploymentId"]
+
+    deployment_result = gg_util_obj.wait_for_deployment_till_timeout(
+        120, deployment_id)
+    if accepted:
+        assert (deployment_result == 'SUCCEEDED')
+    else:
+        assert (deployment_result == 'FAILED')
+
+
+@mark.parametrize("resource,topic,accepted", ACL_TEST_TOPICS + MQTT_TEST_TOPICS)
+def test_Security_6_T2_mqtt_Security_6_T3_mqtt(
+        gg_util_obj: GGTestUtils, system_interface: SystemInterface,
+        resource: str, topic: str, accepted: bool):
+    mqtt_cloud_name = gg_util_obj.upload_component_with_versions(
+        "HelloWorldMqtt", ["1.0.0"])
+    if mqtt_cloud_name is None:
+        raise RuntimeError(
+            "Fatal error: IotMqttPublisher_cloud_name cannot be uploaded to cloud"
+        )
+
+    payload = f"\"Test Component {mqtt_cloud_name}\""
+
+    mqtt_cloud_name = mqtt_cloud_name._replace(
+        merge_config={
+            "accessControl": {
+                "aws.greengrass.ipc.mqttproxy": {
+                    "HelloWorldMqtt:mqttproxy:1": {
+                        "policyDescription":
+                        "access to publish to mqtt topics",
+                        "operations": [
+                            "aws.greengrass#PublishToIoTCore",
+                            "aws.greengrass#SubscribeToIoTCore"
+                        ],
+                        "resources": [resource]
+                    }
+                }
+            },
+            "Topic": topic,
+            "QOS": "1",
+            "Message": payload
+        })
+
+    deployment_id = gg_util_obj.create_deployment(
+        thingArn=gg_util_obj.get_thing_group_arn(config.thing_group_1),
+        component_list=[mqtt_cloud_name],
         deployment_name="FirstDeployment")["deploymentId"]
 
     deployment_result = gg_util_obj.wait_for_deployment_till_timeout(
