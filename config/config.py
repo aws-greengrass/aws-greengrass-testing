498966bbcc78b4988dc7c465e72feb03a8276a4f
Anubhav Rawal <rawalexe@amazon.com>
2025-05-21 21:16:03 +0000

    Add Security test Security_6_T15

--- config/config.py
+++ config/config.py
@@ -1,14 +1,20 @@
 # The AWS account ID in which the test resource will be created.
-aws_account = "YOUR AWS ACCOUNT ID"
+aws_account = "891377305071"
 
 # The S3 bucket used to store the artifacts.
-s3_bucket_name = "THE BUCKET TO STORE ARTIFACTS"
+s3_bucket_name = "gglite-dev-test-us-east-1"
 
 # The region of the AWS Account used for the tests.
-region = "AWS REGION TO RUN THE TESTS IN"
+region = "us-east-1"
 
-# Thing to which all the tests deploy.
-thing_name = "THING NAME"
+# First thing group with 'thing' under test added to it.
+thing_group_1 = "rawalexe-deployment-test"
+
+# Second thing group with 'thing' under test added to it.
+thing_group_2 = "rawalexe-uat-test2"
+
+# Thing which is part of the above thing groups.
+thing_name = "rawalDevDevice"
 
 # Location of ggl-cli binary
 ggl_cli_bin_path = "../aws-greengrass-lite/build/bin/ggl-cli"
